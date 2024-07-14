a748dd4576705ca9be389eabe9d538000bb32f15
Ali Raza Gill <aliraza137725@gmail.com>
2024-07-14 20:43:15 +0500

    feat:MongoDB connected through mongoose with professional approach in detail and also learn error handling

---
+++ src/index.js
@@ -0,0 +1,49 @@
+/*Common errors comes while connecting to db
+import connectDB from "./db";
+Thier is problem in this code, it will give error because we are trying to connect to db before it is ready because the env file is not ready for whole app, so this will give error.
+connectDB();
+*/
+
+//Their is 2 solutions of this problem, one is use here require dotenv and give path of env and another is use import same as our standard comes with all file import standard, we import dotenv file in this file and then config and give the path of env file, and then make changes in the pakage.json file and we use -r flag and then dotenv/config and then write the --experimental-json-modules in the script. we use --experimental-json-modules flag but in future it will be predefined from node.js hopefully.
+
+// 1st Solution is this
+//Solution is this use require dotenv file in this file
+
+/*require("dotenv").config();
+import connectDB from "./db";
+connectDB();
+*/
+
+//
+// 2nd Solution is this, and we use this one.
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+import connectDB from "./db/index.js";
+// we get constants error here if i didnt use extention, .js, so we give extention to cosntact file and db/index.js file
+
+connectDB();
+//now run this comand to start server npm run dev
+
+//
+//----------------------------
+// IIFE approach to connect to db, in the last i use () this is used to call this function imediately.
+(() => {})(); // this syntax is called IIFE.
+
+// This is 1st approach to connect to db, but we didnt use this approach, we use 2nd approach in seperate file for it.
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+    app.on("error", (error) => {
+      console.error("Error connecting to DB with Express", error);
+      throw error;
+    });
+    app.lissten(process.env.PORT, () => {
+      console.log(`App is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to DB", error);
+    throw error;
+  }
+})();
+*/
