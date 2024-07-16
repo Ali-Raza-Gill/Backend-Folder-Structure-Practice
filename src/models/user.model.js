0bdbb90ffe9cea2def939e3d94f97f9419d5dc64
Ali Raza Gill <aliraza137725@gmail.com>
2024-07-16 10:15:54 +0500

    feat:add User and Video Model, and Add how to generate and add working of Access_Token and Refresh_Token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -46,5 +47,51 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+  // this will be called only when password is modified and this is a method to encrypt the password but we use nullified method for password encryption
+  // if (this.isModified("password")) {
+  //   return (this.password = bcrypt.hash(this.password, 10));
+  // }
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//Both AccessToken and RefreshToken are same, no difference at all, only minor difference applies in some cases.
+
+//Here i get the AccessToken with simple function method bcz arrow function is not working.
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      //jwt sign has all the access of all the values of the Mongoose Schema,you can add any value you want here.
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+//Here i get the RefreshToken with simple function method bcz arrow function is not working.
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
